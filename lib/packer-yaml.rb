04e0d0034273bf4825fc744a4fd9cefccc08b49d
mikrob <mikael.robert@botsunit.com>
2016-05-09 19:29:24 +0200

    compare yaml and json format for same descriptor with tests

--- lib/packer-yaml.rb
+++ lib/packer-yaml.rb
@@ -1,5 +1,5 @@
 require 'yaml'
-
+require 'json'
 class PackerYaml
   def self.read file_path
     YAML.load_file(file_path)
--- test/test_load_yaml.rb
+++ test/test_load_yaml.rb
@@ -8,18 +8,16 @@ class TestLoadYaml < Test::Unit::TestCase
 
     def test_load_yaml
       yaml = PackerYaml.read("#{CONFIG_PATH}/packer1.yaml")
-      assert_equal "googlecompute", yaml[:builders][1][:type]
-      assert_equal "amazon-ebs", yaml[:builders][0][:type]
-      assert_equal "#!/bin/bash", yaml[:provisioners].first[:inline].first
-      assert_equal "sudo apt-get -y install ssh", yaml[:provisioners].first[:inline][6]
+      assert_equal "googlecompute", yaml["builders"][1]["type"]
+      assert_equal "amazon-ebs", yaml["builders"][0]["type"]
+      assert_equal "#!/bin/bash", yaml["provisioners"].first["inline"].first
+      assert_equal "sudo apt-get -y install ssh", yaml["provisioners"].first["inline"][6]
     end
 
     def test_yaml_and_json_are_equal
       yaml = PackerYaml.read("#{CONFIG_PATH}/packer1.yaml")
-      json = JSON.prase("#{CONFIG_PATH}/packer1.json")
+      json = JSON.parse(File.read("#{CONFIG_PATH}/packer1.json")) #, {:symbolize_names => true})
 
-      pp yaml
-
-      pp json
+      assert_equal yaml, json
     end
 end
