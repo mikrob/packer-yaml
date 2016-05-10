dab155334361a37c9258d9296396da137e5d83c7
mikrob <mikael.robert@botsunit.com>
2016-05-10 18:29:59 +0200

    add algo to mange include with keeping order

--- lib/packer/yaml.rb
+++ lib/packer/yaml.rb
@@ -1,6 +1,7 @@
 require "packer/yaml/version"
 require 'yaml'
 require 'json'
+require 'pp'
 
 module Packer
   class Yaml
@@ -10,7 +11,47 @@ module Packer
     end
 
     def self.read file_path
-      YAML.load_file(file_path)
+      yaml = YAML.load_file(file_path)
+      yaml.each do |type,content|
+        if type == "variables"
+          include_var = nil
+          content.each do |k,v|
+            if k == :include
+              v = "#{v}.yml" unless v.end_with?(".yml")
+              include_var = YAML.load_file(File.join(File.dirname(file_path), v))
+            end
+          end
+          content.delete(:include)
+          content.merge!(include_var) if include_var
+        else
+          include_content = nil
+          replacements = {}
+          content.each_with_index do |line, index|
+            line.each do |k,v|
+              if k == :include
+                v = "#{v}.yml" unless v.end_with?(".yml")
+                include_content = YAML.load_file(File.join(File.dirname(file_path), v))
+                replacements[index] = include_content
+              end
+            end
+          end
+          replacements.each do |idx, value|
+            if value.is_a? Array
+              content.delete_at(idx)
+              value.each_with_index do |val, idx_val|
+                content.insert(idx + idx_val, val)
+              end
+            else
+              content[idx] = value
+            end
+          end
+        end
+      end
+      yaml
+    end
+
+    def to_hash
+      @yaml
     end
 
     def to_json
@@ -21,4 +62,18 @@ module Packer
       $stdout.write(to_json)
     end
   end
+
+  class Runner
+    def which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable?(exe) && !File.directory?(exe)
+        }
+      end
+      return nil
+    end
+  end
+
 end
