12625acfb5204d66b49f857e9d2b98091d11187a
mikrob <mikael.robert@botsunit.com>
2016-05-11 11:33:17 +0200

    fix cli commands and add a validate task

--- lib/packer/yaml/cli.rb
+++ lib/packer/yaml/cli.rb
@@ -2,16 +2,39 @@ require 'thor'
 
 module Packer
   class Cli < Thor
-    desc "Packer-Yaml a tool to manage packer definition in YAML with includes"
+
+    desc "make_json FILE", "transform given yaml file into valid packer json format"
     long_desc <<-EOF
     Packer-Yaml a tool to manage packer definition in YAML with includes.
     EOF
+    method_option :file, :type => :string
+    def make_json(file)
+      file_name = File.basename(file, ".*" )
+      if File.exist?(file)
+        packer_yaml = Packer::Yaml.new(file_name, file)
+        packer_yaml.output_json
+      else
+        puts "File doesn't exist"
+      end
+    end
 
-    option :file
-    def make_json( name )
-      greeting = "Hello, #{name}"
-      greeting.upcase! if options[:file]
-      puts greeting
+    desc "validate FILE", "transform given yaml file into valid packer json and check it is valid json and valid packer description"
+    long_desc <<-EOF
+    Packer-Yaml a tool to manage packer definition in YAML with includes.
+    EOF
+    method_option :file, :type => :string
+    def validate(file)
+      file_name = File.basename(file, ".*" )
+      if File.exist?(file)
+        packer_yaml = Packer::Yaml.new(file_name, file)
+        if packer_yaml.valid_json?
+          puts "Generated JSON is valid, your YAML is good!"
+        else
+          puts "Generated JSON is not valid"
+        end
+      else
+        puts "Given file doesn't exist"
+      end
     end
 
   end
