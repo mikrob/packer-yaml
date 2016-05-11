a515ef965457d6d27287b4661f46f5fde714167f
mikrob <mikael.robert@botsunit.com>
2016-05-11 11:00:57 +0200

    use thor for cli

--- lib/packer/yaml/cli.rb
+++ lib/packer/yaml/cli.rb
@@ -2,24 +2,18 @@ require 'thor'
 
 module Packer
   class Cli < Thor
-    desc "hello NAME", "This will greet you"
-    long_desc <<-HELLO_WORLD
+    desc "Packer-Yaml a tool to manage packer definition in YAML with includes"
+    long_desc <<-EOF
+    Packer-Yaml a tool to manage packer definition in YAML with includes.
+    EOF
 
-    `hello NAME` will print out a message to the person of your choosing.
-
-    Brian Kernighan actually wrote the first "Hello, World!" program 
-    as part of the documentation for the BCPL programming language 
-    developed by Martin Richards. BCPL was used while C was being 
-    developed at Bell Labs a few years before the publication of 
-    Kernighan and Ritchie's C book in 1972.
-
-    http://stackoverflow.com/a/12785204
-    HELLO_WORLD
-    option :upcase
-    def hello( name )
+    option :file
+    def make_json( name )
       greeting = "Hello, #{name}"
-      greeting.upcase! if options[:upcase]
+      greeting.upcase! if options[:file]
       puts greeting
     end
+
   end
+
 end
